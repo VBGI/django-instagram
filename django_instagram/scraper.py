54e4d24a5a4d4f5eab2f614dfb96f295e6fdfc1f
Dmitry E. Kislov <kislov@easydan.com>
2017-09-02 13:56:27 +1000

    General app restructure

--- django_instagram/scraper.py
+++ django_instagram/scraper.py
@@ -2,58 +2,36 @@
 Created on 04/sep/2016
 
 @author: Marco Pompili
+
+Significantly modified by Dmitry E. Kislov, 2 sept., 2017
+Modifications include:
+    dropping lxml, requests requirements
 """
 
-from lxml import html
-import requests
-import json
+try:
+    from  urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
 
-SCRIPT_JSON_PREFIX = 18
-SCRIPT_JSON_DATA_INDEX = 21
+import json
+import re
+
 
 def instagram_scrap_profile(username):
-    """
-    Scrap an instagram profile page
-    :param username:
-    :return:
-    """
     url = "https://www.instagram.com/{}/".format(username)
-    page = requests.get(url)
-    return html.fromstring(page.content)
-
-
-def instagram_profile_js(username):
-    """
-    Retrieve the script tags from the parsed page.
-    :param username:
-    :return:
-    """
-    tree = instagram_scrap_profile(username)
-    return tree.xpath('//script')
-
-
-def instagram_profile_json(username):
-    """
-    Get the JSON data string from the scripts.
-    :param username:
-    :return:
-    """
-    scripts = instagram_profile_js(username)
-    source = None
-
-    for script in scripts:
-        if script.text:
-            if script.text[0:SCRIPT_JSON_PREFIX] == "window._sharedData":
-                source = script.text[SCRIPT_JSON_DATA_INDEX:-1]
-
-    return source
+    page = urlopen(url)
+    return page.read()
 
 
 def instagram_profile_obj(username):
-    """
-    Retrieve the JSON from the page and parse it to a python dict.
-    :param username:
-    :return:
-    """
-    json_data = instagram_profile_json(username)
-    return json.loads(json_data)
+    pat = re.compile(r'\<script\s?type="text/javascript"\>\s?window\._sharedData\s?=\s?\{(.*?)\};</script>')
+    data = instagram_scrap_profile(username)
+    try:
+        match = pat.findall(data)[-1]
+    except IndexError:
+        match = ''
+    try:
+        jsonified = json.loads(match)
+    except TypeError:
+        jsonified = {}
+    return jsonified
--- django_instagram/templatetags/instagram_client.py
+++ django_instagram/templatetags/instagram_client.py
@@ -2,14 +2,12 @@
 Created on 12/dic/2013
 
 @author: Marco Pompili
+
+Significantly modified by Dmitry E. Kislov, 2 sept., 2017
 """
 
 from django import template
-
-from sorl.thumbnail import get_thumbnail, delete
-
-from django_instagram import settings
-from django_instagram.scraper import instagram_profile_json, instagram_profile_obj
+from django_instagram.scraper import instagram_profile_obj
 
 register = template.Library()
 
@@ -39,7 +37,6 @@ class InstagramUserRecentMediaNode(template.Node):
         return ''
 
 
-
 @register.tag
 def instagram_user_recent_media(parser, token):
     """
@@ -57,6 +54,7 @@ def instagram_user_recent_media(parser, token):
             "%r tag requires a single argument" % token.contents.split()[0]
         )
 
+
 @register.inclusion_tag('django_instagram/recent_media_box.html')
 def instagram_recent_media_box(*args, **kwargs):
     profile = instagram_profile_obj(username=kwargs.get('username'))
@@ -66,32 +64,3 @@ def instagram_recent_media_box(*args, **kwargs):
         'profile': profile,
         'recent_media': recent_media
     }
-
-
-@register.inclusion_tag('django_instagram/recent_media_wall.html')
-def instagram_recent_media_wall(*args, **kwargs):
-    profile = instagram_profile_obj(username=kwargs.get('username'))
-    recent_media = get_profile_media(profile)
-
-    return {
-        'profile': profile,
-        'recent_media': recent_media,
-    }
-
-
-@register.filter(name='standard_size')
-def instagram_standard_size(value):
-    im = get_thumbnail(value, settings.INSTAGRAM_STANDARD_SIZE, crop='center', quality=90)
-    return im.url
-
-
-@register.filter(name='low_resolution')
-def instagram_low_resolution(value):
-    im = get_thumbnail(value, settings.INSTAGRAM_LOW_SIZE, crop='center', quality=90)
-    return im.url
-
-
-@register.filter(name='thumbnail')
-def instagram_thumbnail(value):
-    im = get_thumbnail(value, settings.INSTAGRAM_THUMB_SIZE, crop='center', quality=90)
-    return im.url
