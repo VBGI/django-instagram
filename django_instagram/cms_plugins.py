af304dd8f1e897d89d47c97c4bec9912beba6ed6
Kislov Dmitry <kislov@easydan.com>
2017-09-20 14:32:28 +1000

    indent fixed

--- django_instagram/cms_plugins.py
+++ django_instagram/cms_plugins.py
@@ -4,6 +4,7 @@ from cms.plugin_base import CMSPluginBase
 from .models import Instagram
 from django.utils.translation import ugettext_lazy as _
 from cms.plugin_pool import plugin_pool
+from django.conf import settings
 
 class InstagramPlugin(CMSPluginBase):
     model = Instagram
@@ -16,5 +17,7 @@ class InstagramPlugin(CMSPluginBase):
             'instance': instance})
         return context
 
+    def icon_src(self, instance):
+        return getattr(settings, 'STATIC_URL', '') + "/django_instagram/images/" + "instagram-icon.png"
 
 plugin_pool.register_plugin(InstagramPlugin)
