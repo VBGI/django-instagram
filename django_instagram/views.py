12569f34462413abcf855efac353cee1b817bed1
Kislov Dmitry <kislov@easydan.com>
2017-09-21 14:38:17 +1000

    fix issue with instagram-tags

--- django_instagram/views.py
+++ django_instagram/views.py
@@ -17,6 +17,8 @@ def get_instagram_data(request, username):
     if re.match(r'[a-zA-Z_]+', username) and len(username) < mlength:
         profile = instagram_profile_obj(username=username)
         media = profile['entry_data']['ProfilePage'][-1]['user']['media']['nodes']
+        for k in range(len(media)):
+            media[k]['caption'] = media[k]['caption'].replace('#', ' #')
         return HttpResponse(json.dumps(media), content_type="application/json")
     else:
         return HttpResponse('[]', content_type="application/json")
